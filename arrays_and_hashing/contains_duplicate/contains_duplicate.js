7b904679244ec541ccad06cbb2c4eb41dae2aa6e
Raphael <rewoma19@gmail.com>
2026-01-02 22:05:40 -0700

    Add solution for contains_duplicate.js

--- arrays_and_hashing/contains_duplicate/contains_duplicate.js
+++ arrays_and_hashing/contains_duplicate/contains_duplicate.js
@@ -4,4 +4,25 @@
  * @return {boolean}
  */
 
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  const h = new Set();
+
+  for (const num of nums) {
+    if (h.has(num)) {
+      return true;
+    } else {
+      h.add(num);
+    }
+  }
+
+  return false;
+};
+
+const exampleOne = containsDuplicate([1, 2, 3, 1]);
+console.log(exampleOne);
+
+const exampleTwo = containsDuplicate([1, 2, 3, 4]);
+console.log(exampleTwo);
+
+const exampleThree = containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]);
+console.log(exampleThree);
