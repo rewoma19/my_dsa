158612cf69a61150dde1bcf024631c9f6975e0da
Raphael <rewoma19@gmail.com>
2026-01-04 21:57:15 -0700

    Add solution for two_sum challenge

---
+++ arrays_and_hashing/two_sum/two_sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const h = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    h.set(nums[i], i);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const y = target - nums[i];
+
+    if (h.has(y) && h.get(y) !== i) {
+      return [i, h.get(y)];
+    }
+  }
+};
