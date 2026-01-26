6383906874b8f245d18992001662023cafd51774
Raphael <rewoma19@gmail.com>
2026-01-25 19:35:31 -0700

    Add efficient solution for best_stock_time.js

---
+++ sliding_window/best_stock_time/best_stock_time.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @param {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (const price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    }
+
+    const profit = price - minPrice;
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
+};
